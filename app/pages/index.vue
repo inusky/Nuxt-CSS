ee70eaa3ff0a7c6d969af99b5cc5c6478c5a2495
inusky <inuskydotcom@gmail.com>
2025-11-10 13:56:32 +0530

    Upgrade app to use sass

---
+++ app/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Home</h1>
+</template>
+
+<style lang="scss">
+body {
+  background-color: $app-BG;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    head: {
+      title: 'Nuxt CSS',
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    },
+  },
+  postcss: {
+    plugins: {
+      'postcss-nesting': {},
+      autoprefixer: {},
+    },
+  },
+  vite: {
+    css: {
+      devSourcemap: true,
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/assets/scss/_foundation.scss" as *;',
+        },
+      },
+    },
+  },
+  sourcemap: true,
 })
