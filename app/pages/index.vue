f08e73deaaadd8686b8aa4da7d11967d482beda6
inusky <inuskydotcom@gmail.com>
2025-11-11 12:42:09 +0530

    Experimenting with the flex property and videos: understanding things.

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,14 @@
 <template>
   <h1>Home</h1>
+  <div class="container">
+    <div class="item">Flex item 1</div>
+    <div class="item i2">Flex item 2</div>
+    <div class="item">Flex item 3</div>
+    <div class="item i4">Flex item 4</div>
+    <div class="item i5">Flex item 5</div>
+  </div>
+  <video class="video1" width="640" height="320" controls autoplay muted>
+    <source src="../assets/file_example_WEBM_640_1_4MB.webm" type="video/webm">
+    Sorry, your browser doesn't support embedded videos.
+  </video>
 </template>
