23b06ca79d54fd276ff27584bbb9077ae9710bea
inusky <inuskydotcom@gmail.com>
2025-11-11 18:34:38 +0530

    Experimenting with videos and icons: understanding things

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -8,7 +8,21 @@
     <div class="item i5">Flex item 5</div>
   </div>
   <video class="video1" width="640" height="320" controls autoplay muted>
-    <source src="../assets/file_example_WEBM_640_1_4MB.webm" type="video/webm">
+    <source src="/file_example_WEBM_640_1_4MB.webm" type="video/webm" />
     Sorry, your browser doesn't support embedded videos.
   </video>
+  <p>
+    Video from
+    <NuxtLink to="/">Ishan Nandan</NuxtLink>
+  </p>
+  <img class="img1" src="/image-1.jpg" alt="Image" width="640" height="320" />
+  <p>
+    Image from
+    <NuxtLink to="/">Ishan Nandan</NuxtLink>
+  </p>
+  <img class="img2" src="/icons8-mac-logo-100.png" alt="Icon" />
+  <p>
+    Icon from
+    <NuxtLink to="/">Ishan Nandan</NuxtLink>
+  </p>
 </template>
